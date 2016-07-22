06040c36d472a985f1330e1c7a96ffd218a001bd
Shafique Jamal <shafique.jamal@gmail.com>
2016-07-21 22:01:58 -0400

    Fix spacing

--- test/communication/TestLinkSender.scala
+++ test/communication/TestLinkSender.scala
@@ -5,11 +5,11 @@ import user.User
 class TestLinkSender extends LinkSender {
 
   def send(user: User,
-           host: String,
-           code: String,
-           route: String,
-           subjectMessageKey: String,
-           bodyTextMessageKey: String): Unit = {}
+    host: String,
+    code: String,
+    route: String,
+    subjectMessageKey: String,
+    bodyTextMessageKey: String): Unit = {}
 
 
 }
