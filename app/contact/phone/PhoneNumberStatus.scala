1042c55a981be00c7b36ad498a98caac0301a596
Shafique Jamal <shafique.jamal@gmail.com>
2016-12-04 17:56:46 -0500

    Add phone number statuses

--- app/contact/phone/PhoneNumberStatus.scala
+++ app/contact/phone/PhoneNumberStatus.scala
@@ -1,5 +1,19 @@
 package contact.phone
 
-class PhoneNumberStatus {
-  
+sealed trait PhoneNumberStatus {
+
+  def value: Int
+
+}
+
+object PhoneNumberStatus {
+
+  case object Unverified extends PhoneNumberStatus { override val value = 0 }
+  case object Verified extends PhoneNumberStatus { override val value = 1 }
+  case object Removed extends PhoneNumberStatus { override val value = 2 }
+
+  private val allPhoneNumberStatuses = Seq(Unverified, Verified, Removed)
+
+  def toPhoneNumberStatus(value: Int): PhoneNumberStatus = allPhoneNumberStatuses.find(_.value == value).getOrElse(Removed)
+
 }
